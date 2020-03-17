701819354212268ee2f288592074e853a078fcaa
Michelle Li <limichelle009@gmail.com>
2020-03-16 17:15:48 -0700

    Finished 3/16/2020

--- src/Sports.java
+++ src/Sports.java
@@ -1,4 +1,27 @@
 
-public class Sports {
-	private int[] Player;
+public class Sports{
+	Player [] myplayers;
+	private String sportsType;
+	private String team_individual;
+	
+	public Sports (String n_sportsType, String n_team_individual) {
+		sportsType = n_sportsType;
+		team_individual = n_team_individual;
+	}
+
+	public void loadPlayers(Player[] n_myPlayers) {
+		myplayers = n_myPlayers;
+	}
+
+	public void printRoster() {
+		for (int i = 0; i < myplayers.length; i++) {
+			System.out.println(myplayers[i]);
+	    		myplayers [i].toString();
+		}
+	}
+	
+	public String toString() {
+		return "The sport " + sportsType + " is an " + team_individual + " sport.";
+	}
+
 }
--- src/SportsMain.java
+++ src/SportsMain.java
@@ -6,10 +6,27 @@ public class SportsMain {
 		Player newPlayer = new BasketballPlayer ("Bob", 16, 18, "center", 5);
 		String np1 = newPlayer.toString();
 		System.out.println(np1);
+		System.out.println();
 		
 		//tests equals for Player class
 		Player newPlayer2 = new BasketballPlayer ("Katie", 12, 16, "center", 2);
 		Player newPlayer3 = new BasketballPlayer ("Katie", 12, 16, "center", 2);
 		System.out.println(newPlayer2.equals(newPlayer3));
+		System.out.println();
+		
+		//tests the Sports array
+		Player[] somePlayers = {newPlayer, newPlayer2, newPlayer3};
+		Sports arrayOfPlayers = new Sports ("Basketball", "team");
+		arrayOfPlayers.loadPlayers(somePlayers);
+		System.out.println(arrayOfPlayers.toString());
+		arrayOfPlayers.printRoster();
+		System.out.println();
+		
+		Player[] somePlayers2 = {new Player("Max", 7, "Swimming"), new Player("Jane", 12, "Swimming")};
+		Sports arrayOfPlayers2 = new Sports ("Swimming", "individual");
+		arrayOfPlayers2.loadPlayers(somePlayers2);
+		System.out.println(arrayOfPlayers2.toString());
+		arrayOfPlayers2.printRoster();
+		
 	}
 }
